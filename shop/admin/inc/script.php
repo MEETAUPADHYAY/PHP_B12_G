d37b0c87c7ee370f629b742ace7643fd67afecdc
theeasylearn <theeasylearn@users.noreply.github.com>
2020-10-03 20:31:25 +0530

    theme

---
+++ shop/admin/inc/script.php
@@ -0,0 +1,6 @@
+<!--3rd reusable part-->
+<script src="theme/plugins/jquery/jquery.min.js"></script>
+<script src="theme/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="theme/dist/js/adminlte.min.js"></script>
+<script src="theme/dist/js/demo.js"></script>
+<!--3rd resuable part-->
