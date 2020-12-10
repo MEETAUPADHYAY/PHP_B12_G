29b763afa8f1182ad04b6035f1d0bdf20e855831
theeasylearn <theeasylearn@users.noreply.github.com>
2020-12-10 07:26:23 +0530

    JAVASCRIPT

--- javascript/myscript.js
+++ javascript/myscript.js
@@ -13,27 +13,64 @@ function getValue(id)
 {
     return $(id).value;
 }
+function isDigit(value)
+{
+    const regex = new RegExp(/[^0-9]/,'g');
+    value = String(value);
+    if (value!='' && value.match(regex)) 
+        return false;
+    else 
+        return true;
+}
 function add()
 {
     getInput();
-    var result = num1 + num2;
-    $("output").innerHTML = "result of addition is " + result;
+    if(isDigit(num1)==true && isDigit(num2)==true)
+    {
+        var result = num1 + num2;
+        $("output").innerHTML = "result of addition is " + result;
+    }
+    else
+    {
+        alert("input(s) invalid");
+    }
 }
 function sub()
 {
     getInput();
-    var result = num1 - num2;
-    $("output").innerHTML = "result of substraction is " + result;
+    if(isDigit(num1)==true && isDigit(num2)==true)
+    {
+        var result = num1 - num2;
+        $("output").innerHTML = "result of substraction is " + result;
+    }
+    else
+    {
+        alert("input(s) invalid");
+    }
 }
 function mul()
 {  
     getInput();
-    var result = num1 * num2;
-    $("output").innerHTML = "result of multiplication is " + result;
+    if(isDigit(num1)==true && isDigit(num2)==true)
+    {
+        var result = num1 * num2;
+        $("output").innerHTML = "result of multiplication is " + result;
+    }
+    else
+    {
+        alert("input(s) invalid");
+    }
 }
 function div()
 {
     getInput();
-    var result = num1 / num2;
-    $("output").innerHTML = "result of division is " + result;
+    if(isDigit(num1)==true && isDigit(num2)==true)
+    {
+        var result = num1 / num2;
+        $("output").innerHTML = "result of multiplication is " + result;
+    }
+    else
+    {
+        alert("input(s) invalid");
+    }
 }
